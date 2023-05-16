f443c315a667f2ed41277fe5f43c2ffbb1479498
Ricardo Martínez <theilluminatiduck@gmail.com>
2023-05-16 12:06:53 -0600

    agenda terminado

--- src/Components/AgendaItemComponent.js
+++ src/Components/AgendaItemComponent.js
@@ -1,28 +1,52 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import pfp from '../Assets/small-pfp.png'
+import React from "react";
+import { Link } from "react-router-dom";
+import pfp from "../Assets/small-pfp.png";
+import "../Styles/agenda.component.css";
 
 export default function AgendaItemComponent() {
   return (
-    <div>
-        <input type="month" id="start" name="start"
-       min="2018-03" value="2018-05" />
-       <div className='agenda--items__container'>
-            <div className='agenda--item'>
-                <div className='agenda--text__left'>
-                <p className='day--text'>Dom</p>
-                <p className='day--number'>23</p>
-                </div>
-                <div className='agenda--box'>
-                    <img src={pfp} alt='caca'/>
-                    <div className='row-1'>
-                        <p>Terapia con Alejandro</p>
-                        <p>10:30 - 11:30</p>
-                        <Link></Link>
-                    </div>
-                </div>
+    <div className="agenda--container">
+      <input
+        type="month"
+        id="start"
+        name="start"
+        min="2018-03"
+        value="2018-05"
+      />
+      <div id="containerrr">
+        <div className="agenda--item">
+          <div className="agenda--text__left">
+            <p className="day--text">Dom</p>
+            <p className="day--number">23</p>
+          </div>
+          <div className="agenda--box">
+          <div className="agenda--pfp">
+              <img src={pfp} alt="caca" />
             </div>
-       </div>
+            <div className="row-1">
+              <p>Terapia con Alejandro</p>
+              <p>10:30 - 11:30</p>
+            </div>
+            <Link className="agenda--details__button">Ver detalles</Link>
+          </div>
+        </div>
+        <div className="agenda--item">
+          <div className="agenda--text__left">
+            <p className="day--text">Dom</p>
+            <p className="day--number">23</p>
+          </div>
+          <div className="agenda--box">
+            <div className="agenda--pfp">
+              <img src={pfp} alt="caca" />
+            </div>
+            <div className="row-1">
+              <p>Terapia con Alejandro</p>
+              <p>10:30 - 11:30</p>
+            </div>
+            <Link className="agenda--details__button">Ver detalles</Link>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
