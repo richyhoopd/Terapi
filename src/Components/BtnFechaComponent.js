fd4c1b7caacad5781dfd9c89f232d4884cdf951e
Ricardo Martínez <theilluminatiduck@gmail.com>
2023-05-17 17:08:03 -0600

    lol

--- src/Components/BtnFechaComponent.js
+++ src/Components/BtnFechaComponent.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import calendario from '../Assets/calendar-vector.svg';
 import '../Styles/btnFecha.component.css';
-
+import axios from 'axios';
 
 export default function BtnFechaComponent() {
+  
+  const asdgjkl = async(e)=>{
+    const {value} = (e.target);
+    const {data} = await axios('http://localhost:4001/fecha/'+value);
+
+    console.log(data);
+  }
+
+  /*const submit = async()=>{
+    const fomrData = new FormData();
+    fomrData.append('fecha', fecha);
+    fomrData.append('id_terapist', user._id);
+
+    axios.post('asdgjkl;', fomrData);
+  }*/
+  
   return (
    <div className='btn--container'>
-    <input id='btnFechaComponent' type='date' value="2018-07-22" min="2018-01-01" placeholder='Elige la fecha'/>
+    <input id='btnFechaComponent' type='date' min="2018-01-01" onChange={asdgjkl} placeholder='Elige la fecha'/>
 
    
    </div>
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -7,6 +7,7 @@ import TerapeutasComponent from '../Components/TerapeutasComponent';
 export default function Home() {
   const title = 'Terapia en linea';
   const subtitle = 'Habla con un terapeuta en cualquier lugar totalmente gratis';
+  const [interfaz] = React.useState('home');
 
   return (
     <div>
